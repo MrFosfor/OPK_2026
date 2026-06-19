79ed31fcdb3d9faa4149b8c5a343c72a830bdf04
ShaburovF <f.shaburov@g.nsu.ru>
2026-06-19 08:47:03 +0700

    Add tests for hash_sub and playfair Algo

--- FinalProject/tests/tests.py
+++ FinalProject/tests/tests.py
@@ -16,8 +16,7 @@ def _main_test():
 def _test_cipher(encode_func, decode_func, text, key, language='en'):
     encrypted = encode_func(text, key, language)
     decrypted = decode_func(encrypted, key, language)
-    assert decrypted == text, Fore.LIGHTRED_EX + ("Ошибка для текста '{text}' "
-                                                  "(язык {language}, ключ {key}): '{decrypted}' != '{text}'")
+    assert decrypted == text, f"Ошибка для текста '{text}' (язык {language}, ключ {key}): '{decrypted}' != '{text}'"
 
 
 def _caesar_test():
@@ -59,11 +58,60 @@ def _atbash_test():
 
 
 def _hash_substitution_test():
-    pass
+    _test_cipher(hash_cipher.encode, hash_cipher.decode, "Hello, World!", "key", 'en')
+    _test_cipher(hash_cipher.encode, hash_cipher.decode, "abc", "secret", 'en')
+    _test_cipher(hash_cipher.encode, hash_cipher.decode, "xyz", "pass", 'en')
+    _test_cipher(hash_cipher.encode, hash_cipher.decode, "ABC", "test", 'en')
+    _test_cipher(hash_cipher.encode, hash_cipher.decode, "", "any", 'en')
+    _test_cipher(hash_cipher.encode, hash_cipher.decode, "123!@#", "key", 'en')
+
+    _test_cipher(hash_cipher.encode, hash_cipher.decode, "Привет, мир!", "ключ", 'ru')
+    _test_cipher(hash_cipher.encode, hash_cipher.decode, "абвгдеёжзи", "секрет", 'ru')
+    _test_cipher(hash_cipher.encode, hash_cipher.decode, "ЯЯЯ", "ключ", 'ru')
+    _test_cipher(hash_cipher.encode, hash_cipher.decode, "", "ключ", 'ru')
+
+    text = "Hello"
+    key = "test"
+    enc1 = hash_cipher.encode(text, key, 'en')
+    enc2 = hash_cipher.encode(text, key, 'en')
+    assert enc1 == enc2, (Fore.LIGHTRED_EX +
+                          "Хеш-подстановка должна быть детерминированной при одинаковом ключе")
+
+    enc3 = hash_cipher.encode(text, "other", 'en')
+    assert enc1 != enc3, (Fore.LIGHTRED_EX +
+                          "Разные ключи должны давать разные шифры (хотя бы в большинстве случаев)")
 
 
 def _playfair_test():
-    pass
+    _test_cipher(playfair.encode, playfair.decode, "HELLO", "KEY", 'en')
+    _test_cipher(playfair.encode, playfair.decode, "WORLD", "SECRET", 'en')
+    _test_cipher(playfair.encode, playfair.decode, "ATTACK", "MONARCHY", 'en')
+    _test_cipher(playfair.encode, playfair.decode, "TEST", "CIPHER", 'en')
+    _test_cipher(playfair.encode, playfair.decode, "A", "SINGLE", 'en')
+    _test_cipher(playfair.encode, playfair.decode, "AA", "DOUBLE", 'en')
+    _test_cipher(playfair.encode, playfair.decode, "ABCDE", "KEY", 'en')
+    _test_cipher(playfair.encode, playfair.decode, "", "ANY", 'en')
+
+    _test_cipher(playfair.encode, playfair.decode, "ПРИВЕТ", "КЛЮЧ", 'ru')
+    _test_cipher(playfair.encode, playfair.decode, "МИР", "СЕКРЕТ", 'ru')
+    _test_cipher(playfair.encode, playfair.decode, "ААА", "КЛЮЧ", 'ru')
+    _test_cipher(playfair.encode, playfair.decode, "АБВГД", "КЛЮЧ", 'ru')
+    _test_cipher(playfair.encode, playfair.decode, "", "КЛЮЧ", 'ru')
+    _test_cipher(playfair.encode, playfair.decode, "А,Б.В-Г/Д", "КЛЮЧ", 'ru')
+
+    _test_cipher(playfair.encode, playfair.decode, "HELLO", "A", 'en')
+    _test_cipher(playfair.encode, playfair.decode, "ПРИВЕТ", "Я", 'ru')
+
+    text = "HELLO"
+    key = "KEY"
+    enc1 = playfair.encode(text, key, 'en')
+    enc2 = playfair.encode(text, key, 'en')
+    assert enc1 == enc2, (Fore.LIGHTRED_EX +
+                          "Плейфер должен быть детерминирован при одинаковом ключе")
+
+    enc3 = playfair.encode(text, "OTHER", 'en')
+    assert enc1 != enc3, (Fore.LIGHTRED_EX +
+                          "Разные ключи должны давать разные шифры (в большинстве случаев)")
 
 
 def tests():
